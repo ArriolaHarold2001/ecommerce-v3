6c6b29b9b749d4f9113c1015b292032774779975
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-03-20 12:23:05 -0400

    final

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-const SQL_PORT = process.env.SQL_PORT;
+const PORT = process.env.PORT;
 
-app.listen(SQL_PORT, () => {
-  console.log(`Listening on port ${SQL_PORT}`);
+app.get("/", (req, res) => {
+  res.status(200).send("server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
