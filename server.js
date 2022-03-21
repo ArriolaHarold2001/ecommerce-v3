9de632121b7189db7df0215aa9d78c66ecef62de
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-03-21 13:45:04 -0400

    working on backend

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql");
+const axios = require("axios");
 require("dotenv").config();
+
+const db = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "password",
+  database: "ecommerce",
+});
 
 const app = express();
 app.use(express.json());
@@ -9,7 +18,12 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.status(200).send("server");
+  db.query("SELECT * FROM products", (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.status(200).send(result);
+  });
 });
 
 app.listen(PORT, () => {
