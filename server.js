35e3f496318c202412692f2528d62a25abc1a1ad
Harold Arriola Nucamendi <arriolaharold2001@gmail.com>
2022-03-23 18:42:13 -0400

    debugging server

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
 app.get("/api/products", (req, res) => {
   db.query("SELECT * FROM products", (err, result) => {
     if (err) {
+      console.log(err);
       throw err;
     }
     res.status(200).json(result);
@@ -37,6 +38,7 @@ app.get("/api/featured-products", (req, res) => {
     "SELECT * FROM products INNER JOIN featured_products ON products.id = featured_products.product_id",
     (err, result) => {
       if (err) {
+        console.log(err);
         throw err;
       }
       res.status(200).json(result);
@@ -47,6 +49,7 @@ app.get("/api/featured-products", (req, res) => {
 app.get("/api/slider", (req, res) => {
   db.query("SELECT * FROM slider", (err, result) => {
     if (err) {
+      console.log(err);
       throw err;
     }
     res.status(200).send(result);
@@ -56,6 +59,7 @@ app.get("/api/slider", (req, res) => {
 app.get("/api/drop-filter", (req, res) => {
   db.query("SELECT * FROM drop_filter", (err, result) => {
     if (err) {
+      console.log(err);
       throw err;
     }
     res.status(200).send(result);
